0a3b81ea870c72a2445e69d8caf4f89efdb92a51
Stas9364 <stas-lisovskii@mail.ru>
2022-04-27 01:06:22 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Counter} from "./Counter/Counter";
+import style from "./Counter/Counter.module.css";
 
 function App() {
+    let [count, setCount] = useState<number>(0);
+
+    const addStyle = style.add;
+    const resetStyle = style.reset;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <Counter
+            setCount={setCount}
+            count={count}
+            addStyle={addStyle}
+            resetStyle={resetStyle}
+        />
     </div>
   );
 }
